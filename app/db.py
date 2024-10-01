73f5620e57498ab5917ce6a4e8247eda4697e60b
mewtoex <144196543+mewtoex@users.noreply.github.com>
2024-10-01 08:29:41 -0300

    ----

--- app/db.py
+++ app/db.py
@@ -1,5 +1,8 @@
 from supabase import create_client, Client
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
