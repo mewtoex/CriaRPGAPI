db03be31fef6c6d00cc51c6721db895b804b0057
mewtoex <144196543+mewtoex@users.noreply.github.com>
2024-10-05 03:00:46 -0300

    Melhorada chamda de tecnica para pega todas

--- controler/tecnica/query_builder.py
+++ controler/tecnica/query_builder.py
@@ -18,25 +18,45 @@ def update_tecnica(tecnica):
 
 def tecnica_list_filter(pre):
     query = supabase.table('tecnicas').select("*")
-
-    if pre.get('chassi') is not None: 
+    if not isinstance(pre, dict):
+        return []
+ 
+    if 'chassi' in pre and pre['chassi'] is not None:
         chassi = pre.get('chassi', '')
         query = query.or_(
             f"chassi.like.%{chassi}%,chassi.eq.''"
         )
-    
-    if pre.get('elemento') is not None:  
+    if 'elemento' in pre and pre['elemento'] is not None:
         elemento = pre.get('elemento', '')
         query = query.or_(
             f"elemento.like.%{elemento}%,elemento.eq.''"
         )
-        
-    if pre.get('rank') is not None: 
+    if 'rank' in pre and pre['rank'] is not None:
         rank = pre.get('rank', '')
         query = query.or_(
             f"rank.like.%{rank}%,rank.eq.''"
         )
 
-    response = query.execute()
-    return response.data
+    if 'chassi' in pre and pre['chassi'] is not None:
+        chassi = pre.get('chassi', '')
+        query_chassi_and_elemento_null = supabase.table('tecnicas').select("*")\
+        .eq('chassi', chassi)\
+        .is_('elemento', None)
+
+    if 'elemento' in pre and pre['elemento'] is not None:
+        elemento = pre.get('elemento', '')
+        query_elemento_and_chassi_null = supabase.table('tecnicas').select("*")\
+        .eq('elemento', elemento)\
+        .is_('chassi', None)
+   
+        query_prerequisito_null1 = supabase.table('tecnicas').select("*")\
+        .eq('prerequisito', 0)\
+
+    
+    response_filter = query.execute()
+    response_chassi = query_chassi_and_elemento_null.execute()
+    response_pre = query_prerequisito_null1.execute()
+    response_elemento = query_elemento_and_chassi_null.execute()
+    response = response_pre.data + response_chassi.data + response_elemento.data + response_filter.data
+    return response
 
